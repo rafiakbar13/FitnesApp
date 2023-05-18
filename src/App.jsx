bf30ff712714a6d393dae128f15b45d1655a595b
rafiakbar13 <muhammadrafia13@gmail.com>
2023-05-18 14:04:43 +0700

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      <h1 className=''>React 18 Alpha</h1>
+    </div>
+  )
+}
+
+export default App
